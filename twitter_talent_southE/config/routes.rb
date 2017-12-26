6d505fbee794e7b2520275eaf1b4be08c3f7390e
MARY KAYE COONS <MKCOONS9@GMAIL.COM>
2017-12-26 09:14:09 -0600

    first commit

---
+++ twitter_talent_southE/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root 'epicenter#feed'
+
+  get 'show_user' => 'epicenter#show_user'
+
+  get 'now_following' => 'epicenter#now_following'
+
+  get 'unfollow' => 'epicenter#unfollow'
+
+  get 'tag_tweets' => 'epicenter#tag_tweets'
+
+  get 'all_users' => 'epicenter#all_users'
+  
+  resources :tweets
+  devise_for :users
+end
